d620c58e9ead8df969c88f6608eca15479ccc77d
Emily Asch <e.asch@me.com>
2018-10-15 18:45:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,5 +2,27 @@
 def begins_with_r(array)
   array.each {|word| return false if word.start_with?("r") == false}
   true
+  end
 end
+
+def contain_a(array)
+  array.select{|word| word.include? ("a")}
+end
+
+def first_wa(array)
+  array.detect {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|element| element.class != String}
+end
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.each {|element| counts[element] += 1 }
+  returnarr= []
+  counts.each do |element,num|
+    element.each {|key, val| return_arr << {key => val, :count => num}}
+  end
+  returnarr
 end
